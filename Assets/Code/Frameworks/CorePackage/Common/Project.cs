b37ea10054ded6b97f91b094a9320ffc86765fae
Wagner GFX <16827309+WagnerGFX@users.noreply.github.com>
2022-08-26 18:44:42 -0300

    Common: Added a simple documentation

--- Assets/Code/Frameworks/CorePackage/Common/Project.cs
+++ Assets/Code/Frameworks/CorePackage/Common/Project.cs
@@ -1,6 +1,9 @@
 
 namespace CorePackage.Common
 {
+    /// <summary>
+    /// Holds project-wide constants
+    /// </summary>
     public class Project
     {
         public const string MenuName = "MyProject";
