904cbe5ea551dc5e2e72961aae3606dfbceefca4
Wagner GFX <16827309+WagnerGFX@users.noreply.github.com>
2023-04-29 20:50:02 -0300

    Extensions: Improved code style
    - Applied naming conventions
    - Simplified code logic to be more compact and easy to read.

--- Assets/CorePackage/Utilities/Extensions/VectorExtensions.cs
+++ Assets/CorePackage/Utilities/Extensions/VectorExtensions.cs
@@ -2,28 +2,19 @@ using UnityEngine;
 
 public static class VectorExtensions
 {
-    private static readonly Vector3[] compass3D = { Vector3.left, Vector3.right, Vector3.up, Vector3.down, Vector3.forward, Vector3.back };
-    private static readonly Vector2[] compass2D = { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
+    private static readonly Vector3[] _compass3D = { Vector3.left, Vector3.right, Vector3.up, Vector3.down, Vector3.forward, Vector3.back };
+    private static readonly Vector2[] _compass2D = { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
 
     /// <summary>
     /// Collapse each axis to be either 1, 0 or -1. With optional epsilon precision.
     /// </summary>
     public static Vector3 CollapseAxisToSign(this Vector3 vector, float epsilon = 0f)
     {
-        if (Mathf.Abs(vector.x) > epsilon)
-            vector.x = Mathf.Sign(vector.x);
-        else
-            vector.x = 0f;
+        vector.x = Mathf.Abs(vector.x) > epsilon ? Mathf.Sign(vector.x) : 0f;
 
-        if (Mathf.Abs(vector.y) > epsilon)
-            vector.y = Mathf.Sign(vector.y);
-        else
-            vector.y = 0f;
+        vector.y = Mathf.Abs(vector.y) > epsilon ? Mathf.Sign(vector.y) : 0f;
 
-        if (Mathf.Abs(vector.z) > epsilon)
-            vector.z = Mathf.Sign(vector.z);
-        else
-            vector.z = 0f;
+        vector.z = Mathf.Abs(vector.z) > epsilon ? Mathf.Sign(vector.z) : 0f;
 
         return vector;
     }
@@ -33,39 +24,24 @@ public static class VectorExtensions
     /// </summary>
     public static Vector2 CollapseAxisToSign(this Vector2 vector, float epsilon = 0f)
     {
-        if (Mathf.Abs(vector.x) > epsilon)
-            vector.x = Mathf.Sign(vector.x);
-        else
-            vector.x = 0f;
+        vector.x = Mathf.Abs(vector.x) > epsilon ? Mathf.Sign(vector.x) : 0f;
 
-        if (Mathf.Abs(vector.y) > epsilon)
-            vector.y = Mathf.Sign(vector.y);
-        else
-            vector.y = 0f;
+        vector.y = Mathf.Abs(vector.y) > epsilon ? Mathf.Sign(vector.y) : 0f;
 
         return vector;
     }
-
 
     /// <summary>
     /// Returns true if the vector is one of the six cardinal directions.
     /// </summary>
     public static bool IsCardinal(this Vector3 vector)
     {
-        if (vector == Vector3.right)
-            return true;
-        if (vector == Vector3.left)
-            return true;
-        if (vector == Vector3.up)
-            return true;
-        if (vector == Vector3.down)
-            return true;
-        if (vector == Vector3.forward)
-            return true;
-        if (vector == Vector3.back)
-            return true;
-
-        return false;
+        return vector == Vector3.right
+                || vector == Vector3.left
+                || vector == Vector3.up
+                || vector == Vector3.down
+                || vector == Vector3.forward
+                || vector == Vector3.back;
     }
 
     /// <summary>
@@ -73,19 +49,13 @@ public static class VectorExtensions
     /// </summary>
     public static bool IsCardinal(this Vector2 vector)
     {
-        if (vector == Vector2.right)
-            return true;
-        if (vector == Vector2.left)
-            return true;
-        if (vector == Vector2.up)
-            return true;
-        if (vector == Vector2.down)
-            return true;
-
-        return false;
+        return vector == Vector2.right
+            || vector == Vector2.left
+            || vector == Vector2.up
+            || vector == Vector2.down;
     }
 
-    
+
     /// <summary>
     /// Returns the closest of the six cardinal direction.
     /// </summary>
@@ -94,7 +64,7 @@ public static class VectorExtensions
         float maxDotProduct = -Mathf.Infinity;
         Vector3 returnValue = Vector3.zero;
 
-        foreach (Vector3 cardinalDir in compass3D)
+        foreach (Vector3 cardinalDir in _compass3D)
         {
             float dotProduct = Vector3.Dot(direction, cardinalDir);
 
@@ -104,7 +74,7 @@ public static class VectorExtensions
                 maxDotProduct = dotProduct;
             }
         }
- 
+
         return returnValue;
     }
 
@@ -116,7 +86,7 @@ public static class VectorExtensions
         float maxDotProduct = -Mathf.Infinity;
         Vector2 returnValue = Vector2.zero;
 
-        foreach (Vector2 cardinalDir in compass2D)
+        foreach (Vector2 cardinalDir in _compass2D)
         {
             float dotProduct = Vector2.Dot(direction, cardinalDir);
 
